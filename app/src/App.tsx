584bbcb43cd4b282d79ff77be5d8443dffd60011
Maksim1509 <maaa23@mail.ru>
2023-03-15 22:04:53 +0400

    feat: setup tests

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,18 +2,20 @@ import Aboutpage from './pages/Aboutpage/Aboutpage';
 import Homepage from './pages/Homepage/Homepage';
 import Notfoundpage from './pages/Notfoundpage/Notfoundpage';
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Layout from './components/Layout/Layout';
 
 const App = () => (
-  <Routes>
-    <Route path="/" element={<Layout />}>
-      <Route index element={<Homepage />} />
-      <Route path="about" element={<Aboutpage />} />
-      <Route path="*" element={<Notfoundpage />} />
-    </Route>
-  </Routes>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Homepage />} />
+        <Route path="about" element={<Aboutpage />} />
+        <Route path="*" element={<Notfoundpage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
---
+++ app/src/__tests__/App.test.tsx
@@ -0,0 +1,11 @@
+// Imports
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+
+describe('App', () => {
+  it('render App component', () => {
+    render(<App />);
+    expect(screen.getByText(/About us/i)).toBeInTheDocument();
+  });
+});
