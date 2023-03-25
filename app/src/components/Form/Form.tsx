a5ef302992ba4479e389c9e2db1bc4f94b71283e
Maksim1509 <maaa23@mail.ru>
2023-03-25 21:23:30 +0400

    feat: add form

---
+++ app/src/components/Form/Form.tsx
@@ -0,0 +1,42 @@
+import React, { FormEvent } from 'react';
+import './Form.scss';
+
+class Form extends React.Component {
+  constructor(props = {}) {
+    super(props);
+  }
+
+  handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+      <form className="form" onSubmit={this.handleSubmit}>
+        <input type="text" name="name" placeholder="Name" />
+        <input type="date" name="date" />
+        <select name="city">
+          <option value="saratov">Saratov</option>
+          <option value="moscow">Moscow</option>
+          <option value="sochi">Sochi</option>
+        </select>
+        <label>
+          Male
+          <input type="radio" name="gender" value="male" />
+        </label>
+        <label>
+          Female
+          <input type="radio" name="gender" value="female" />
+        </label>
+        <input type="file" name="file" />
+        <label>
+          I consent to my personal data
+          <input type="checkbox" name="personal-checkbox" />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+
+export default Form;
--- app/src/pages/FormPage/FormPage.tsx
+++ app/src/pages/FormPage/FormPage.tsx
@@ -1,10 +1,14 @@
+import Form from '../../components/Form/Form';
 import React from 'react';
 import './FormPage.scss';
 
 const FormPage = () => {
   return (
-    <section className="form">
-      <h2 className="form__title">Form</h2>
+    <section className="form-page">
+      <h2 className="form-page__title">Form</h2>
+      <section className="form-page__form">
+        <Form />
+      </section>
     </section>
   );
 };
