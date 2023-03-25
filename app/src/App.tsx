9e9c414360764fdf17d5226c1916c7bbeccba167
Maksim1509 <maaa23@mail.ru>
2023-03-25 19:36:58 +0400

    refactor: add redirect to page  404

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,7 +2,7 @@ import Aboutpage from './pages/Aboutpage/Aboutpage';
 import Homepage from './pages/Homepage/Homepage';
 import Notfoundpage from './pages/Notfoundpage/Notfoundpage';
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Layout from './components/Layout/Layout';
 
@@ -11,7 +11,8 @@ const App = () => (
     <Route path="/" element={<Layout />}>
       <Route index element={<Homepage />} />
       <Route path="about" element={<Aboutpage />} />
-      <Route path="*" element={<Notfoundpage />} />
+      <Route path="404" element={<Notfoundpage />} />
+      <Route path="*" element={<Navigate to={'404'} />} />
     </Route>
   </Routes>
 );
