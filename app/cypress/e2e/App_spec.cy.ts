d5604ff9ad7de92e639e3b2015583380589da7e4
Maksim1509 <maaa23@mail.ru>
2023-05-02 21:13:32 +0400

    feat: add tests

--- app/cypress/e2e/App_spec.cy.ts
+++ app/cypress/e2e/App_spec.cy.ts
@@ -1,7 +1,7 @@
 describe('App e2e', () => {
   it('should have a searh input', () => {
     cy.visit('/');
-
+    cy.get('.home__loading').should('have.text', 'Loading...');
     cy.get('input').should('have.value', '');
     cy.get('h1').should('have.text', 'Home');
     cy.get('input').type('text');
@@ -35,12 +35,18 @@ describe('App e2e', () => {
   it('should have a card', () => {
     cy.visit('/');
     cy.get('.card__container').should('have.class', 'card__container');
+    cy.get('.home__loading').should('not.exist');
   });
   it('should open a modal', () => {
+    cy.intercept('https://rickandmortyapi.com/api/character/1').as('fetchCard');
     cy.visit('/');
     cy.get('.card__container:first').click();
     cy.get('.modal-card').should('exist');
-    cy.get('.modal__close').click();
+    cy.contains('LOADING...').should('exist');
+    cy.wait('@fetchCard');
+    cy.contains('Status').should('exist');
+    cy.contains('LOADING...').should('not.exist');
+    cy.get('.modal__close').click({ force: true });
     cy.get('.modal-card').should('not.exist');
   });
   it('active link', () => {
