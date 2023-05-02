bfbeaa3e143d180e49130a571547f7b753fe8dd1
Maksim1509 <maaa23@mail.ru>
2023-05-02 19:33:51 +0400

    feat: add tests

--- app/cypress/e2e/Form_spec.cy.ts
+++ app/cypress/e2e/Form_spec.cy.ts
@@ -3,5 +3,25 @@ describe('Form e2e', () => {
     cy.visit('/form');
     cy.get('input[name=name]').type('Name');
     cy.get('input[name=name]').should('have.value', 'Name');
+    cy.get('input[name=date]').type('2023-01-01');
+    cy.get('input[name=date]').should('have.value', '2023-01-01');
+    cy.get('select').select('other');
+    cy.get('select').should('have.value', 'other');
+    cy.get('input[type=radio]').first().check();
+    cy.get('input[type=radio]').first().should('be.checked');
+    cy.get('input[type=file]').selectFile({
+      contents: Cypress.Buffer.from('file contents'),
+      fileName: 'file.jpg',
+      lastModified: Date.now(),
+    });
+    cy.get('input[type=checkbox]').first().check();
+    cy.get('input[type=checkbox]').should('be.checked');
+    cy.get('form').submit();
+    cy.get('.form-card').should('exist');
+    cy.get('input[name=name]').should('have.value', '');
+    cy.get('input[name=date]').should('have.value', '');
+    cy.get('select').select('-- select --');
+    cy.get('input[type=radio]').first().should('not.be.checked');
+    cy.get('input[type=checkbox]').should('not.be.checked');
   });
 });
